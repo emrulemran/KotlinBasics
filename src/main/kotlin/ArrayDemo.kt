fb0eaffd86284a2c43290903c93b909698c1dbb5
bdinfotech2022 <bdinfotech2022@gmail.com>
2023-02-14 13:18:49 -0500

    arrays added

---
+++ src/main/kotlin/ArrayDemo.kt
@@ -0,0 +1,10 @@
+import com.sun.xml.internal.fastinfoset.util.StringArray
+
+fun main(args: Array<String>) {
+    var names = arrayOf("dallas", "miami", "chicago", "boston")
+
+    for(x in names){
+        println(x)
+    }
+
+}
