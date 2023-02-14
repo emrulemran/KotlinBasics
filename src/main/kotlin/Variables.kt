ccdf2ca1f233f68868460797e20367d21ec271e3
bdinfotech2022 <bdinfotech2022@gmail.com>
2023-02-14 12:51:40 -0500

    variables added

--- src/main/kotlin/Variables.kt
+++ src/main/kotlin/Variables.kt
@@ -1,5 +1,17 @@
 fun main(args: Array<String>) {
 
-var city:String = "New York City"
+
+// string variable:
+    var city: String = "New York City"
     println(city)
+
+// var = mutable variable and val = immutable variable
+
+
+    var myNum:Int = 5             // Int
+    val myDoubleNum:Double = 5.99    // Double
+    val myLetter:Char = 'D'        // Char
+    val myBoolean:Boolean = true      // Boolean
+    val myText:String = "Hello"      // String
+
 }
