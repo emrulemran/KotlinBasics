4af6b9e31fc0e7b61fed677a83b75491bfbc2f23
bdinfotech2022 <bdinfotech2022@gmail.com>
2023-02-14 13:11:47 -0500

    variables added

--- src/main/kotlin/Variables.kt
+++ src/main/kotlin/Variables.kt
@@ -7,9 +7,16 @@ fun main(args: Array<String>) {
 
 // var = mutable variable and val = immutable variable
 
+// numeric types:
 
-    var myNum:Int = 5             // Int
+    var myByte:Byte = 100
+    var myShort:Short = 23412
+    val num:Int = 123232
     val myDoubleNum:Double = 5.99    // Double
+    val lnum:Long = 1234567890L
+    var fval:Float = 123456.34567F
+    var fvalue:Float = 123456.34567f
+
     val myLetter:Char = 'D'        // Char
     val myBoolean:Boolean = true      // Boolean
     val myText:String = "Hello"      // String
